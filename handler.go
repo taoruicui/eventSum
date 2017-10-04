e9108c533633d86c4190d15cbb8621d85fe029f3
Jonathan Wen <jonathanwen95@gmail.com>
2017-10-04 15:17:29 -0700

    Added handling for exception details

--- handler.go
+++ handler.go
@@ -4,7 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"errors"
 	"log"
+	"strconv"
+	"time"
 )
 
 // Base class for handling HTTP Requests
@@ -13,12 +16,28 @@ type httpHandler struct {
 	log *log.Logger
 }
 
+type ExceptionDetailsResult struct {
+	dateCreated time.Time
+	dateUpdated time.Time
+	exceptionType string
+	message string
+	count int
+	function string
+	path string
+	stacktrace StackTrace
+	data map[string]interface{}
+}
+
 // Writes an error to ResponseWriter
 func (h *httpHandler) sendError(w http.ResponseWriter, code int, err error, message string) {
 	errMsg := fmt.Sprintf("%s: %s", message, err.Error())
 	h.log.Println(errMsg)
 	w.WriteHeader(code)
 	w.Write([]byte(errMsg))
+}
+
+func (h *httpHandler) sendResp(w http.ResponseWriter, key string, val string) {
+
 }
 
 func (h *httpHandler) recentExceptionsHandler(w http.ResponseWriter, r *http.Request) {
@@ -33,12 +52,60 @@ func (h *httpHandler) detailsExceptionsHandler(w http.ResponseWriter, r *http.Re
 	}
 
 	query := r.URL.Query()
-
-	if str := query.Get("exception_id"); str == "" {
-		//h.sendError(w, http.StatusBadRequest, error("Exception ID is missing"), "")
+	if str := query.Get("exception_id") == ""; str {
+		h.sendError(w, http.StatusBadRequest, errors.New("Exception ID is missing"), "Error")
 		return
 	}
-	fmt.Println(query)
+	exceptionId, err := strconv.Atoi(query.Get("exception_id"))
+	if err != nil {
+		h.sendError(w, http.StatusBadRequest, errors.New("Exception ID is not an int"), "Error")
+		return
+	}
+	if str := query.Get("exception_data_id") == ""; str {
+		h.sendError(w, http.StatusBadRequest, errors.New("Exception Data ID is missing"), "Error")
+		return
+	}
+	exceptionDataId, err := strconv.Atoi(query.Get("exception_data_id"))
+	if err != nil {
+		h.sendError(w, http.StatusBadRequest, errors.New("Exception Data ID is not an int"), "Error")
+		return
+	}
+	projectId := query.Get("project")
+	if projectId == "" {
+		h.sendError(w, http.StatusBadRequest, errors.New("Project ID is missing"), "Error")
+		return
+	}
+	fmt.Println(exceptionId, exceptionDataId)
+	// Query the DB
+	var exceptionInstance []ExceptionInstance
+	var exceptionData []ExceptionData
+	var exceptionPeriod []ExceptionInstancePeriod
+	exceptionInstance = append(exceptionInstance, ExceptionInstance{})
+	exceptionData = append(exceptionData, ExceptionData{})
+	exceptionPeriod = append(exceptionPeriod, ExceptionInstancePeriod{})
+	exceptionInstance[0].Id = int64(exceptionId)
+	exceptionData[0].Id = int64(exceptionDataId)
+
+	h.es.ds.QueryExceptionInstances(exceptionInstance)
+	h.es.ds.QueryExceptionData(exceptionData)
+	exceptionPeriod[0].ExceptionInstanceId = exceptionInstance[0].Id
+	exceptionPeriod[0].ExceptionDataId = exceptionData[0].Id
+	// Process result
+	response := ExceptionDetailsResult {}
+	//	dateCreated: ,
+	//	exceptionType: ,
+	//	message: ,
+	//	count: ,
+	//	function: ,
+	//	path:,
+	//	stacktrace: ,
+	//	data: ,
+	//}
+
+	fmt.Println(exceptionInstance)
+	js, _ := json.Marshal(response)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
 
 func (h *httpHandler) histogramExceptionsHandler(w http.ResponseWriter, r *http.Request) {
